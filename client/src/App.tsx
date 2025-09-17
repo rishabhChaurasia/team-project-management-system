f478dfbe84d0ab8347a8c5ed5d7a370c3ebb1360
Rishabh <rishabhsingh.33333@gmail.com>
2025-09-17 18:25:08 +0530

    shadcn ui components added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import { Toaster } from "./components/ui/sonner";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Toaster />
+    </>
   );
 };
 
